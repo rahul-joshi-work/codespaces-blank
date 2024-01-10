bf1d9a46032c3fade0f52fb3bcb858dcd67e30b6
Rahul Joshi <136357574+rahul-joshi-work@users.noreply.github.com>
2024-01-10 15:49:46 +0000

    Today is done 1)Dataconversion.js 2)Comparison_Datatypes.js

---
+++ Basic_Javascript/01_Variable.js
@@ -0,0 +1,40 @@
+// let name = "Shivaji";
+// const surname = "Maharaj";
+// var fullname = "Sayaji";
+// let Mail = "rahulhotmail888@gmail.com";
+
+// console.log("Name = " + name);
+// console.log("Surname = " + surname);
+// console.log("Fullname = " + fullname);
+// console.log("Mail = " + Mail);
+
+// console.table([name,surname,fullname,Mail]);
+// console.table([name,surname,fullname,Mail]);
+
+
+// let name = "ram";
+// let surname = "Vishva";
+// let city = "Jamnager";
+// console.log([name,surname,city]);
+
+
+// let email = "tiger@gmail.com";
+// let phone = "sivam";
+// console.table([email,phone]);
+
+// let firstname = "shivam";
+// let lastname = "pandya";
+// console.table([firstname, lastname]);
+
+let firstname = "rahul";
+let lastname = "Joshi";
+let salary = 10000;
+let college = "Gurukul";
+let region = "Junagadh";
+// console.table([firstname, lastname, salary, college, region]);
+
+let all = (firstname+" "+lastname+" "+salary+" "+college+" "+region);
+for(let a=0; a < all.length-32; a++)
+{    
+    console.log("All = " + all);
+}
--- Javascript_Programs/two.js
+++
@@ -1,3 +0,0 @@
-array = [10,20,30,'ABC'];
-console.log("Output is = " + array[1]);
-console.log("Output is = " + array);
