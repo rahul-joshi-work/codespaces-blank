9fd8c9839c26f9efe39bae40e6132d8a994d08ae
Rahul Joshi <136357574+rahul-joshi-work@users.noreply.github.com>
2024-01-12 14:14:14 +0000

    all_dates_methods

---
+++ Basic_Javascript/06_Nums_Mathes.js
@@ -0,0 +1,24 @@
+// const score = 400;
+// console.log(score);
+// const Balance = new Number("Balance_String");
+// console.log(typeof Balance);
+// console.log(Balance);  
+
+// const otherNumber = 123.8975;
+// console.log(otherNumber.toPrecision(4));
+// console.log(typeof otherNumber);
+
+// const OneLakha = 100000;
+// console.log(OneLakha.toLocaleString("en-IN"));
+
+console.log("Absulate_Values -3 = " + Math.abs(-3));
+console.log("Round 4.7 = " + Math.round(4.7));
+console.log("Ceil 4.8 = " + Math.ceil(4.8));
+console.log("Floor 4.5 = " + Math.floor(4.5));
+console.log("Max = " + Math.max(1,7,8,2));
+console.log("Min = " + Math.min(1,5,4,7,8));
+console.log("Random = " + Math.floor(Math.random()*10) + 1);
+
+const min = 10;t
+const max = 20;
+console.log(Math.floor(Math.random()*(max - min + 1)));
