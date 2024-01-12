2828111a30e25e739efff77de2ec73d4a836f978
Rahul Joshi <136357574+rahul-joshi-work@users.noreply.github.com>
2024-01-12 14:14:59 +0000

    dates_with_methos

--- Basic_Javascript/06_Nums_Mathes.js
+++ Basic_Javascript/06_Nums_Mathes.js
@@ -11,14 +11,15 @@
 // const OneLakha = 100000;
 // console.log(OneLakha.toLocaleString("en-IN"));
 
-console.log("Absulate_Values -3 = " + Math.abs(-3));
-console.log("Round 4.7 = " + Math.round(4.7));
-console.log("Ceil 4.8 = " + Math.ceil(4.8));
-console.log("Floor 4.5 = " + Math.floor(4.5));
-console.log("Max = " + Math.max(1,7,8,2));
-console.log("Min = " + Math.min(1,5,4,7,8));
-console.log("Random = " + Math.floor(Math.random()*10) + 1);
+// console.log("Absulate_Values -3 = " + Math.abs(-3));
+// console.log("Round 4.7 = " + Math.round(4.7));
+// console.log("Ceil 4.8 = " + Math.ceil(4.8));
+// console.log("Floor 4.5 = " + Math.floor(4.5));
+// console.log("Max = " + Math.max(1,7,8,2));
+// console.log("Min = " + Math.min(1,5,4,7,8));
+// console.log("Random = " + Math.floor(Math.random()*10) + 1);
 
-const min = 10;t
-const max = 20;
-console.log(Math.floor(Math.random()*(max - min + 1)));
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random()*(max - min + 1)));
+
---
+++ Basic_Javascript/07_Data.js
@@ -0,0 +1,20 @@
+// let myDate = new Date();
+// console.log(myDate);
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+// let myDate1 = new Date(2023, 1, 30);
+// console.log(myDate1.toDateString());
+
+// let newDate = new Date();
+// console.log(newDate.getTime());
+// console.log(newDate.getDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth());
+// console.log(newDate.getHours());
+// console.log(newDate.getMilliseconds());
+
