3185d2ff8928426370501c015bf2af3d9431496f
Rahul Joshi <136357574+rahul-joshi-work@users.noreply.github.com>
2024-01-15 15:08:26 +0000

    ar

--- Basic_Javascript/08_array.js
+++ Basic_Javascript/08_array.js
@@ -1,7 +1,7 @@
-const Myarray1 = [0,1,2,3,4,5];
-const Myarray2 = ["Saktiman","Shivaji","Hulk"];
-const Myarray3 = new Array("abc","def","hij","jkl");
-const Myarray4 = new Array("Mari","Chadti","Bogharani","Ma","Pata","Mareche");
+// const Myarray1 = [0,1,2,3,4,5];
+// const Myarray2 = ["Saktiman","Shivaji","Hulk"];
+// const Myarray3 = new Array("abc","def","hij","jkl");
+// const Myarray4 = new Array("Mari","Chadti","Bogharani","Ma","Pata","Mareche");
 // console.log(Myarray1);
 // console.log(Myarray2);
 // console.log(Myarray3);
@@ -19,8 +19,37 @@ const Myarray4 = new Array("Mari","Chadti","Bogharani","Ma","Pata","Mareche");
 // Myarray4.pop();
 // console.log(Myarray4);
 
-const Myarray5 = new Array("hi","hello","by","near","by","me");
-console.log(Myarray5.unshift(10));
-console.log(Myarray5);
+// const Myarray5 = new Array("hi","hello","by","near","by","me");
+// Myarray5.unshift("Hum Tumare Hai");
+// console.log(Myarray5);
+
+// const Myarray6 = new Array("Ek","Tha","Salman");
+// Myarray6.shift();
+// Myarray6.shift();
+// console.log(Myarray6);
+
+// const Myarray7 = new Array("Nayo","Lagta","Dil","Mera");
+// Myarray7.shift(2);
+// console.log(`Array = Myarray7`);
+// console.log(`Include = ${Myarray7.includes("forthis")}`);
+// console.log(`IndexOf = ${Myarray7.indexOf(5)}`);
 
 
+// const Myarray8 = new Array("One","Two","Three","Four");
+// const Myarray9 = new Array("Kay","Lap","Chap","Nai");
+// const NewOne = Myarray8.join(Myarray9);
+// console.log(NewOne);
+
+// const Myarray10 = new Array("Chinta","Mani","Sirf");
+// const Myarray11 = new Array("Lokik","Sukha","Deti","hai");
+// const Newtwo = Myarray10.join(Myarray11);
+
+const Myarray12 = new Array("ram","shyam","Ghanshyam","shri","ram","jai");
+console.log("Main Array 1 = " + Myarray12);
+const Myarray13 = Myarray12.slice(0,3);
+console.log("Slice = " + Myarray13);
+
+const Myarray14 = new Array("Abnormal","Paisa","Pachad","Bhagto");
+console.log("Main Array 2 = " + Myarray14);     
+const Myarray15 = Myarray14.splice(0,4);
+console.log("splice = " + Myarray15);     
