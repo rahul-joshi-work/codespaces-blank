1de2a09952f4cde246fac40c8f8f3b1290eb0969
Rahul Joshi <136357574+rahul-joshi-work@users.noreply.github.com>
2024-01-15 13:34:31 +0000

    arrays

---
+++ Basic_Javascript/08_array.js
@@ -0,0 +1,26 @@
+const Myarray1 = [0,1,2,3,4,5];
+const Myarray2 = ["Saktiman","Shivaji","Hulk"];
+const Myarray3 = new Array("abc","def","hij","jkl");
+const Myarray4 = new Array("Mari","Chadti","Bogharani","Ma","Pata","Mareche");
+// console.log(Myarray1);
+// console.log(Myarray2);
+// console.log(Myarray3);
+// console.log(Myarray4);
+
+// Myarray4.push(6);
+// console.log(Myarray4);
+
+// Myarray4.pop()
+// console.log(Myarray4);
+
+// Myarray4.pop();
+// console.log(Myarray4);
+
+// Myarray4.pop();
+// console.log(Myarray4);
+
+const Myarray5 = new Array("hi","hello","by","near","by","me");
+console.log(Myarray5.unshift(10));
+console.log(Myarray5);
+
+
