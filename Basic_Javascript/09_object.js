a542b722ec93a4745000107efaab412ba02d0c1a
Rahul Joshi <136357574+rahul-joshi-work@users.noreply.github.com>
2024-01-17 15:10:48 +0000

    hjkj

---
+++ Basic_Javascript/09_object.js
@@ -0,0 +1,91 @@
+// const JSuser = {
+//     Name:"rahul",
+//     Surname:"joshi",
+//     Address: "Junagadh"
+// }
+// console.log(JSuser["Name"]);
+// console.log(JSuser[Surname]);
+// console.log(JSuser.Name);
+// console.log(JSuser.Surname);
+// console.log(JSuser.Address);
+// change the values
+// console.log(JSuser.Name = "Hi I am rahul");
+// console.log(JSuser.Surname = "Priyakhant");
+// console.log(JSuser.Address = "Valsaad");
+
+
+
+// const one = {
+//     one: "one",
+//     two: "two",
+//     three: "three"
+// }
+// console.log(one["one"]);
+// console.log(one["two"]);
+// console.log(one["three"]);
+// console.log(one.one);
+// console.log(one.two);
+// console.log(one.three);
+// change the values
+// console.log(one.one = "Ohh this is one");
+// console.log(one.two = "this is an two");
+// console.log(one.three = "this is an three");
+
+
+
+// const three = {
+//     A:"Aone",
+//     B:"Bone",
+//     C:"Cone"
+// }
+// console.log(three["A"]);
+// console.log(three["B"]);
+// console.log(three["C"]);
+// console.log(three.A);
+// console.log(three.B);
+// console.log(three.C);
+// with change the values
+// console.log(three.A = "oh one");
+// console.log(three.B = "oh two");
+// console.log(three.C = "oh three");
+
+
+// example:1 symbol with object
+// const symbol = Symbol("key1");
+// const obj1 = {
+//     Name : "rahul",
+//     Surname : "mishra",
+//     Location : "Ahemdabad",
+//     [symbol]: "key1"
+// }
+//console.log(obj1["symbol"]);
+// console.log(obj1.Name = "this is an rahul");
+// console.log(obj1.Surname = "this is an joshi");
+// console.log(obj1.Location = "Junagadh Location");
+// console.log(obj1[symbol] = "this is an key symbol");
+
+
+
+
+
+// example: 2 with symbol and object.
+// const symbol = Symbol("Oh Shit Done");
+// //console.log(typeof symbol);
+// const obj2 = {
+//     Name : "pooname",
+//     surname : "shivashtav",
+//     [symbol] : "this is an symbol"
+// }
+// console.log(obj2[symbol]);
+
+
+
+// //example : 3 object with symbol
+// const symbol = Symbol("Ohe Ohe");
+// const obj3 = {
+//     This : "this",
+//     That : "that",
+//     Me : "me",
+//     [symbol] : "this an symbol"
+// }
+// console.log(obj3[symbol]);
